5733af513f9ff4490bff8f2923c2f1d5ee0eebb0
Aleksandra Didenko <didenko0a@yandex.ru>
2026-08-24 10:35:26 +0300

    Implement Morse converter server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+
+	srv := server.NewServer(logger)
+
+	if err := srv.Start(); err != nil {
+		logger.Fatal(err)
+	}
 }
